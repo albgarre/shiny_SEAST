e4bd1b06c5fc33085d3eef3d57c2def17c55fc0b
= <=>
2016-05-02 17:26:57 +0200

    Ejercicio terminado

--- server.R
+++ server.R
@@ -3,6 +3,7 @@
 library(shiny)
 library(dplyr)
 library(lubridate)
+library(ggplot2)
 
 ##-----------------------------------------------------------------------------
 
@@ -11,6 +12,27 @@ shinyServer(function(input, output) {
     ##-------------------------------------------------------------------------
     
     ##TAREA2: Rellenar para que tweets_frame sea reactivo
+        
+    tweets_frame <- reactive({
+      in_file <- input$in_file  # Accedemos al input
+      
+      if (is.null(in_file))
+        return(NULL)
+      
+      file_path <- in_file$datapath  # in_file es una lista. El path esta en datapath.
+      
+      read.table(file_path, header = TRUE,sep = "\t", stringsAsFactors = FALSE) %>%
+        
+                               mutate(.,
+                                      anno = year(fecha),
+                                      mes = month(fecha),
+                                      dia = day(fecha),
+                                      hora = hour(fecha),
+                                      minuto = minute(fecha),
+                                      hora0 = (dia - min(dia)) * 24 + hora + minuto/60
+                                      )
+                                      
+                            })
     
     ##-------------------------------------------------------------------------
     
@@ -23,38 +45,37 @@ shinyServer(function(input, output) {
     ##---------------------------------------------------------------------
     
     output$tweets <- renderTable({
-        
-        in_file <- input$in_file  # Accedemos al input
-        
-        if (is.null(in_file))
-            return(NULL)
-        
-        file_path <- in_file$datapath  # in_file es una lista. El path esta en datapath.
-        
-        tweets_frame <- read.table(file_path, header = TRUE,
-                                   sep = "\t", stringsAsFactors = FALSE) %>%
-            mutate(.,
-                   anno = year(fecha),
-                   mes = month(fecha),
-                   dia = day(fecha),
-                   hora = hour(fecha),
-                   minuto = minute(fecha),
-                   hora0 = (dia - min(dia)) * 24 + hora + minuto/60
-            )
-        
+              
         ##---------------------------------------------------------------------
         
         ##TAREA1: Cambiar para que el numero de filas lo elija el usuario.
         
         ##---------------------------------------------------------------------
         
-        return(head(tweets_frame, 5))
+        return(head(tweets_frame(), input$in_number))
 
     })
     
     ##-------------------------------------------------------------------------
     
     ##TAREA2: Implementar grafico de evolucion de tweets
+    
+    output$grafica <- renderPlot({
+      
+      copia <- tweets_frame()
+      copia <- filter(copia, fecha>input$restriccion[1] & fecha<input$restriccion[2])
+      
+      
+      mi_tabla <- copia 
+      # mi_tabla$fechagraf <- cut(ymd_hms(mi_tabla$fecha), input$barra)
+      mi_tabla$hora0 <- cut(mi_tabla$hora0, input$barra)
+      
+      # ggplot(data=mi_tabla, aes(x=fechagraf, color = animo, fill=animo)) +
+      ggplot(data=mi_tabla, aes(x=hora0, color = animo, fill=animo)) +
+        geom_bar(position = "dodge")
+      
+    })
+    
     
     ##-------------------------------------------------------------------------
     
--- ui.R
+++ ui.R
@@ -8,18 +8,20 @@ shinyUI(
     
     ## Barra de navegacion
     
-    navbarPage("Titulo",
+    navbarPage("#GoTSeason6",
                
                ## Panel 1
                
-               tabPanel("Panel 1",
+               tabPanel("Data Input",
                         sidebarLayout(
                             sidebarPanel(
-                                fileInput("in_file", "Selecciona archivo")
+                                fileInput("in_file", "Selecciona archivo"),
                                 
                                 ##---------------------------------------------
                                 
                                 ##TAREA1: Implementar numericInput para numero de filas
+                                
+                                numericInput("in_number", "Numero de tweets", "0")
                                 
                                 ##---------------------------------------------
                                 
@@ -32,19 +34,25 @@ shinyUI(
                
                ## Panel 2
                
-               tabPanel("Panel 2",
+               tabPanel("Resultados",
                         sidebarLayout(
                             sidebarPanel(
                                 ##---------------------------------------------
                                 
                                 ##TAREA2: Implementar dateRangeInput
                                 
+                                sliderInput("barra", "Numero de pares de barras", 2, 20, 2),
+                                dateRangeInput("restriccion", "Tweets mostrados en el gráfico de barras")
+                                
                                 ##---------------------------------------------
                                 ),
+                            
                             mainPanel(
                                 ##---------------------------------------------
                                 
                                 ##TAREA2: Implementar grafico de evolucion
+                                
+                                plotOutput("grafica")
                                 
                                 ##---------------------------------------------
                                 )
