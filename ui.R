3ae296de6886e183dc0a70e4ef03c31c1c960630
= <=>
2016-05-02 17:43:15 +0200

    Ejercicio shiny terminado
    Pilar

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 
 
 library(shiny)
+library(ggplot2)
 
 #------------------------------------------------------------------------------
 
@@ -8,18 +9,21 @@ shinyUI(
     
     ## Barra de navegacion
     
-    navbarPage("Titulo",
+    navbarPage("#GoTSeason6",
                
                ## Panel 1
                
-               tabPanel("Panel 1",
+               tabPanel("Data Input",
                         sidebarLayout(
                             sidebarPanel(
-                                fileInput("in_file", "Selecciona archivo")
+                                fileInput("in_file", "Selecciona archivo"),
                                 
                                 ##---------------------------------------------
                                 
                                 ##TAREA1: Implementar numericInput para numero de filas
+                                
+                                numericInput("in_number", "número de filas", 1)
+                                
                                 
                                 ##---------------------------------------------
                                 
@@ -32,21 +36,32 @@ shinyUI(
                
                ## Panel 2
                
-               tabPanel("Panel 2",
+               tabPanel("Resultados",
                         sidebarLayout(
                             sidebarPanel(
                                 ##---------------------------------------------
                                 
                                 ##TAREA2: Implementar dateRangeInput
                                 
+                                
+                                sliderInput("num_barra", "número de barras", 2, 20, 2),
+                                dateRangeInput("restriccion", "limite")
                                 ##---------------------------------------------
                                 ),
                             mainPanel(
+                              plotOutput("fecha")
+                              
                                 ##---------------------------------------------
                                 
                                 ##TAREA2: Implementar grafico de evolucion
                                 
                                 ##---------------------------------------------
+                              
+                              
+                              
+                              
+                              
+                              
                                 )
                         )
                )
