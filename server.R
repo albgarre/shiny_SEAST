ad2affd8e6f0d3c6ec52d8b44e0ea8fd1a4f2ee9
Sergio Lopez <sergiolopezm5@users.noreply.github.com>
2017-05-26 18:44:38 +0200

    Practica terminada

--- server.R
+++ server.R
@@ -2,62 +2,78 @@
 
 library(shiny)
 library(dplyr)
+library(ggplot2)
 library(lubridate)
 
 ##-----------------------------------------------------------------------------
 
 shinyServer(function(input, output) {
+  
+  ##-------------------------------------------------------------------------
+  
+  ##TAREA2: Rellenar para que tweets_frame sea reactivo
+  
+  n_tweet <- reactive(input$n_tweet)
+  ##-------------------------------------------------------------------------
+  
+  ## Tabla de tweets
+  
+  ##---------------------------------------------------------------------
+  
+  ##TAREA2: Modificar para que trabaje con expresión reactiva
+  
+  ##---------------------------------------------------------------------
     
-    ##-------------------------------------------------------------------------
-    
-    ##TAREA2: Rellenar para que tweets_frame sea reactivo
-    
-    ##-------------------------------------------------------------------------
-    
-    ## Tabla de tweets
+    tweets_frame <- reactive({
+      
+      in_file <- input$in_file  # Accedemos al input
+      
+      if (is.null(in_file))
+        return(NULL)
+      
+      file_path <- in_file$datapath  # in_file es una lista. El path esta en datapath.
+      
+      
+      read.table(file_path,
+                 header = TRUE,
+                 sep = "\t",
+                 stringsAsFactors = FALSE) %>% 
+        mutate(.,
+             anno = year(fecha),
+             mes = month(fecha),
+             dia = day(fecha),
+             hora = hour(fecha),
+             minuto = minute(fecha),
+             hora0 = (dia - min(dia)) * 24 + hora + minuto/60)
+      })
     
     ##---------------------------------------------------------------------
     
-    ##TAREA2: Modificar para que trabaje con expresión reactiva
+    ##TAREA1: Cambiar para que el numero de filas lo elija el usuario.
     
     ##---------------------------------------------------------------------
-    
-    output$tweets <- renderTable({
-        
-        in_file <- input$in_file  # Accedemos al input
-        
-        if (is.null(in_file))
-            return(NULL)
-        
-        file_path <- in_file$datapath  # in_file es una lista. El path esta en datapath.
-        
-        tweets_frame <- read.table(file_path, header = TRUE,
-                                   sep = "\t", stringsAsFactors = FALSE) %>%
-            mutate(.,
-                   anno = year(fecha),
-                   mes = month(fecha),
-                   dia = day(fecha),
-                   hora = hour(fecha),
-                   minuto = minute(fecha),
-                   hora0 = (dia - min(dia)) * 24 + hora + minuto/60
-            )
-        
-        ##---------------------------------------------------------------------
-        
-        ##TAREA1: Cambiar para que el numero de filas lo elija el usuario.
-        
-        ##---------------------------------------------------------------------
-        
-        return(head(tweets_frame, 5))
 
+    output$tweets <- renderTable({
+      return(head(tweets_frame(), n_tweet()))
     })
+  
+  ##-------------------------------------------------------------------------
+  
+  ##TAREA2: Implementar grafico de evolucion de tweets
+  
+  output$graf <- renderPlot({ 
     
-    ##-------------------------------------------------------------------------
+    grafico <- tweets_frame() %>%
+      filter(fecha>input$daterange[1] & fecha<input$daterange[2]) %>%
+      mutate (hora0 = cut(hora0, input$slider)) %>%
+      ggplot(aes(x = hora0 ,fill = animo)) +
+      geom_bar(position = "dodge")
     
-    ##TAREA2: Implementar grafico de evolucion de tweets
-    
-    ##-------------------------------------------------------------------------
-    
+    return(grafico)
+  })  
+  
+  ##-------------------------------------------------------------------------
+  
 })
 
 
--- ui.R
+++ ui.R
@@ -5,53 +5,64 @@ library(shiny)
 #------------------------------------------------------------------------------
 
 shinyUI(
-    
-    ## Barra de navegacion
-    
-    navbarPage("Titulo",
-               
-               ## Panel 1
-               
-               tabPanel("Panel 1",
-                        sidebarLayout(
-                            sidebarPanel(
-                                fileInput("in_file", "Selecciona archivo")
-                                
-                                ##---------------------------------------------
-                                
-                                ##TAREA1: Implementar numericInput para numero de filas
-                                
-                                ##---------------------------------------------
-                                
-                            ),
-                            mainPanel(
-                                tableOutput("tweets")
-                            )
+  
+  ## Barra de navegacion
+  
+  navbarPage("#GoTSeason6",
+             
+             ## Panel 1
+             
+             tabPanel("Data Input",
+                      sidebarLayout(
+                        sidebarPanel(
+                          fileInput("in_file", "Selecciona archivo"),
+                          
+                          ##---------------------------------------------
+                          
+                          ##TAREA1: Implementar numericInput para numero de filas
+                          numericInput("n_tweet", "Número de Tweets:", 5, min = 1, max = 100)
+                          
+                          ##---------------------------------------------
+                          
+                        ),
+                        mainPanel(
+                          tableOutput("tweets")
                         )
-               ),
-               
-               ## Panel 2
-               
-               tabPanel("Panel 2",
-                        sidebarLayout(
-                            sidebarPanel(
-                                ##---------------------------------------------
-                                
-                                ##TAREA2: Implementar dateRangeInput
-                                
-                                ##---------------------------------------------
-                                ),
-                            mainPanel(
-                                ##---------------------------------------------
-                                
-                                ##TAREA2: Implementar grafico de evolucion
-                                
-                                ##---------------------------------------------
-                                )
+                      )
+             ),
+             
+             ## Panel 2
+             
+             tabPanel("Resultados",
+                      sidebarLayout(
+                        sidebarPanel(
+                          ##---------------------------------------------
+                          
+                          ##TAREA2: Implementar dateRangeInput
+                          
+                          dateRangeInput("daterange", "Limites",
+                                         start = "2016-04-18",
+                                         end   = "2016-04-30"),
+                          
+                          sliderInput("slider", "Número breaks", min = 2, 
+                                      max = 20, value = 5)
+                          
+                          
+                          ##---------------------------------------------
+                        ),
+                        mainPanel(
+                          ##---------------------------------------------
+                          
+                          ##TAREA2: Implementar grafico de evolucion
+                          
+                          plotOutput("graf")
+                          
+                          ##---------------------------------------------
                         )
-               )
-               
-    )  ##-- Fin navbarPage #GoTSeason6
+                      )
+             )
+             
+  )  ##-- Fin navbarPage #GoTSeason6
 )
 
 
